433223d710d263c711f46fac28b748217883bf73
nishana95 <nishananoufiya@gmail.com>
2025-11-22 19:01:19 +0530

    first commit

---
+++ List/list1.py
@@ -0,0 +1,40 @@
+# list1= [1,2,"Python","Program",15.9]
+# list2=["Amy","Ryan","Henry","Emma"]
+# print(list1)
+# print(list2)
+
+# print(list1[0])
+# print(list2[2])
+
+# print(list1[-1])
+# print(list2[-3])
+# list=["p","r","o","g","i","m"]
+# print(list[2:5])
+# print(list[5:])
+# print(list[:])
+
+# numbers=[21,34,54,12]
+# print("before append:",numbers)
+# numbers.append(50)
+# print("after append:",numbers)
+
+# numbers.insert(3,11)
+# print(numbers)
+# list2=[2,4,6,8]
+# list1.extend(list2)
+# print(list1)
+# prime_numbers=[2,3,5,7]
+# removed_element=prime_numbers.pop(2)
+# print('removed element:',removed_element)
+# print('updated list:',prime_numbers)
+
+list1=[12,14,16,18,20]
+# l=list1*2
+# print(l)
+list2=[9,10,32,54,86]
+# l=list1+list2
+# print(l)
+a=len(list1)
+print(a)
+
+
